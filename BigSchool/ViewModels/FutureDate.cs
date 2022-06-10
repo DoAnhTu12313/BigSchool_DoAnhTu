9a2a2f9cfe903ff393c488b9d426ad47785e775a
DoAnhTu12313 <tudo01032001@gmail.com>
2022-06-10 11:14:05 +0700

    Nop bai tuan 2 10/6

--- BigSchool/ViewModels/FutureDate.cs
+++ BigSchool/ViewModels/FutureDate.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +9,14 @@ namespace BigSchool.ViewModels
 {
     public class FutureDate : ValidationAttribute  
     {
-        public override bool IsVaild(object value)
+        public override bool IsValid(object value)
         {
+            DateTime datetime;
+            var isVailid = DateTime.TryParseExact(Convert.ToString(value), "dd/M/yyyy",
+                CultureInfo.CurrentCulture, DateTimeStyles.None,
+                out datetime);
 
+            return (isVailid && datetime  > DateTime.Now);
         }
     }
 }
---
+++ BigSchool/ViewModels/ValidTime.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool.ViewModels
+{
+    public class ValidTime : ValidationAttribute
+    {
+        public override bool IsValid (object value)
+        {
+            DateTime datetime;
+            var isvalid = DateTime.TryParseExact(Convert.ToString(value), "HH:mm",
+                CultureInfo.CurrentCulture, DateTimeStyles.None, out datetime);
+            return isvalid;
+        }
+    }
+}

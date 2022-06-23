bd97133a89bdef2494314c529df3a2394c3f0122
DoAnhTu12313 <tudo01032001@gmail.com>
2022-06-23 23:33:20 +0700

    Em nộp bổ sung bài cuối

---
+++ BigSchool/Controllers/AttendancesController.cs
@@ -0,0 +1,34 @@
+﻿using BigSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace BigSchool.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _dbContext;
+
+        public AttendancesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody] int courseID)
+        {
+            var attendance = new Attendance
+            {
+                CourseID = courseID,
+                AttendeeId = User.Indentity.GetUserId()
+            };
+            _dbContext.Attendances.Add(attendance);
+            _dbContext.SaveChanges();
+            return Ok();
+    }
+}
